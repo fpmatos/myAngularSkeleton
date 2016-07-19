aa811c188747a6351a8b4567d124f9bcdfc53d70
fernando.prudente <fernandosotam@gmail.com>
2016-07-19 18:04:08 -0300

    inclusao de suite de teste

---
+++ js/service.js
@@ -0,0 +1,9 @@
+angular.module("app", []);
+
+angular.module("app").controller("controller1", function () {
+    alert(1);
+})
+
+angular.module("app").service("service1", function () {
+    alert(1);
+})   
---
+++ test/specs/service.spec.js
@@ -0,0 +1,5 @@
+describe("Teste 1", function () {
+    it("ddad", function () {
+        expect(3).toBe(2);
+    }) 
+})
